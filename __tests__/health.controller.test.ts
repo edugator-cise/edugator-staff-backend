ef666fc23861ec8864fa769c091b2a3ece5da676
Marc Diaz <marcgabe15@gmail.com>
2021-09-11 19:36:19 -0400

    fix ci hang

--- __tests__/health.controller.test.ts
+++ __tests__/health.controller.test.ts
@@ -2,6 +2,13 @@ import app from '../src/index';
 import * as request from 'supertest';
 
 describe('GET /v1/health', () => {
+  beforeAll((done: jest.DoneCallback) => {
+    done()
+  })
+
+  afterAll((done: jest.DoneCallback) => {
+    done()
+  })
   it('health api request', async () => {
     const result: request.Response = await request(app).get('/v1/health');
     expect(result.statusCode).toEqual(200);
