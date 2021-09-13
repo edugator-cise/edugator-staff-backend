b5fd972f5e45e630afa174341d5ba86510b306bb
Dhruv Patel <dhruv2000patel@gmail.com>
2021-09-13 17:42:57 -0400

    Added new validation file

---
+++ src/validation/module.validation.ts
@@ -0,0 +1,15 @@
+import Joi from 'joi';
+import JoiObjectId from 'joi-objectid';
+const objectIdValidator = JoiObjectId(Joi);
+
+const moduleValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().min(1).required(),
+    number: Joi.number().min(1).required(),
+    problem: Joi.array().items(objectIdValidator()).required()
+  });
+
+  return schema.validate(data);
+};
+
+module.exports.moduleValidation = moduleValidation;
