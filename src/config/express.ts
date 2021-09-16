8ceb35c4e057c071b84d350159dfb5071265d8ea
Marc Diaz <33824219+marcgabe15@users.noreply.github.com>
2021-09-16 11:04:22 -0400

    Marcgabe15/test deployment (#22)
    * add ci check
    
    * fix workflow
    
    * fix workflow again
    
    * add health route
    
    * fix ci hang
    
    * linter
    
    * check what's happening with github ci
    
    * fix workflow again
    
    * fix open hang again
    
    * Finally fixed hanging
    
    * add collection name
    
    * test deployment
    
    * new run id
    
    * retry
    
    * instantiate another deployment pipeline
    
    * add release deployment and extra test for index

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { Request, Response } from 'express';
 import routes from '../api/routes/v1';
 
 const app = express();
@@ -8,4 +9,8 @@ app.use(express.json());
 // mount api v1 routes
 app.use('/v1', routes);
 
+app.get('/', (_req: Request, res: Response): void => {
+  // use static 200 to prevent undefined message from http-status
+  res.status(200).send('OK');
+});
 export default app;
