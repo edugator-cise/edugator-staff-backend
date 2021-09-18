0fa31d100900efd85010e3655dde5dcbaf54ac5a
Marc Diaz <marcgabe15@gmail.com>
2021-09-17 22:58:55 -0400

    add cors

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import { Request, Response } from 'express';
 import routes from '../api/routes/v1';
-
+import * as cors from 'cors';
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 // mount api v1 routes
 app.use('/v1', routes);
