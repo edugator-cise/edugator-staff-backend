b3aed623599ed412af4213d989059d897979e6cc
Hamish Pierpont <hamish.pierpont@ufl.edu>
2021-09-30 12:46:19 -0400

    Fixed server-crashing TypeError in problem.validation.ts

--- src/api/validation/problem.validation.ts
+++ src/api/validation/problem.validation.ts
@@ -1,29 +1,37 @@
-import * as Joi from 'joi';
+import {
+  ValidationResult,
+  object,
+  string,
+  boolean,
+  date,
+  array,
+  number
+} from 'joi';
 
 //eslint-disable-next-line
-const problemValidation = (data: any): Joi.ValidationResult => {
-  const schema = Joi.object({
-    problemType: Joi.string().required(),
-    title: Joi.string().min(1).required(),
-    hidden: Joi.boolean().required(),
-    language: Joi.string().min(1).required(),
-    dueDate: Joi.date().iso().required(),
+const problemValidation = (data: any): ValidationResult => {
+  const schema = object({
+    problemType: string().required(),
+    title: string().min(1).required(),
+    hidden: boolean().required(),
+    language: string().min(1).required(),
+    dueDate: date().iso().required(),
     code: {
-      header: Joi.string().min(1).required(),
-      body: Joi.string().min(1).required(),
-      footer: Joi.string().min(1).required()
+      header: string().min(1).required(),
+      body: string().min(1).required(),
+      footer: string().min(1).required()
     },
-    fileExtension: Joi.string().valid('.java', '.cpp', '.h').min(1).required(),
-    testCases: Joi.array().items({
-      input: Joi.string().min(1).required(),
-      expectedOutput: Joi.string().min(1).required(),
-      hint: Joi.string().min(1).required(),
-      visibility: Joi.number().valid(0, 1, 2).required(),
-      templatePackage: Joi.string().min(1).required()
+    fileExtension: string().valid('.java', '.cpp', '.h').min(1).required(),
+    testCases: array().items({
+      input: string().min(1).required(),
+      expectedOutput: string().min(1).required(),
+      hint: string().min(1).required(),
+      visibility: number().valid(0, 1, 2).required(),
+      templatePackage: string().min(1).required()
     }),
-    timeLimit: Joi.number().required(),
-    memoryLimit: Joi.number().required(),
-    buildCommand: Joi.string().min(1).required()
+    timeLimit: number().required(),
+    memoryLimit: number().required(),
+    buildCommand: string().min(1).required()
   });
 
   return schema.validate(data);
