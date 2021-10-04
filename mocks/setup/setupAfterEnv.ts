de399536025e81418380dbcdfb5e88bf9c6372eb
Marc Diaz <marcgabe15@gmail.com>
2021-10-04 17:34:49 -0400

    fix merge conflict

--- mocks/setup/setupAfterEnv.ts
+++ mocks/setup/setupAfterEnv.ts
@@ -1,8 +1,4 @@
 import * as databaseHelper from '../../src/config/database';
-<<<<<<< HEAD
-
-=======
->>>>>>> cd8428220c893397aa496ac3cb2e8606cd9543d7
 process.env.NODE_ENV = 'test';
 //eslint-disable-next-line
 beforeAll(() => {
