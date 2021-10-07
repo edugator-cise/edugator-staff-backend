81ff202149c96d4de616d8936e8bd9529bc42710
Dhruv Patel <dhruv2000patel@gmail.com>
2021-10-07 16:23:25 -0400

    Module Route Testing Complete, needs review

--- src/api/controllers/module.controller.ts
+++ src/api/controllers/module.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { Types } from 'mongoose';
 import { Module, ModuleDocument } from '../models/module.model';
 import { Problem } from '../models/problem.model';
 
@@ -22,19 +23,23 @@ export const getModuleByID = async (
 ): Promise<void> => {
   let modules: ModuleDocument;
   try {
-    //Find exact problem if the moduleId is given in the params
-    //The .select command excludes the problems field from the response
-    if (req.params.moduleId) {
-      // Find One Module
-      modules = await Module.findOne({
-        _id: req.params.moduleId
-      }).select('-problems');
+    const ObjectId = Types.ObjectId;
+    if (!ObjectId.isValid(req.params.moduleId)) {
+      throw { message: 'This route requires a valid module ID' };
+    }
+
+    // Find One Module
+    modules = await Module.findOne({
+      _id: req.params.moduleId
+    }).select('-problems');
+
+    if (modules != null) {
       res.status(200).send(modules);
     } else {
-      res.status(400).send('This route requires a query param of an moduleId');
+      res.status(400).send({ message: 'Module not found in database' });
     }
   } catch (err) {
-    res.status(400).type('json').send(err);
+    res.status(400).send(err);
   }
 };
 
@@ -70,7 +75,16 @@ export const postModules = async (
 
 export const putModule = async (req: Request, res: Response): Promise<void> => {
   // makes sure there is a moduleId given in the params
-  if (req.params.moduleId) {
+  try {
+    const ObjectId = Types.ObjectId;
+    if (!ObjectId.isValid(req.params.moduleId)) {
+      throw { message: 'This route requires a valid module ID' };
+    }
+
+    if (Object.keys(req.body).length === 0) {
+      throw { message: 'This route requires a body to be passed in' };
+    }
+
     const module = await Module.findByIdAndUpdate(
       {
         _id: req.params.moduleId
@@ -78,16 +92,17 @@ export const putModule = async (req: Request, res: Response): Promise<void> => {
       req.body,
       { new: true }
     ).select('-problems');
+
     if (module) {
       res.status(200).type('json').send(module);
     } else {
-      res.status(404).type('json').send({ error: 'Module not found' });
+      res
+        .status(400)
+        .type('json')
+        .send({ message: 'Module not found in database' });
     }
-  } else {
-    res
-      .status(404)
-      .type('json')
-      .send({ error: 'No moduleId given in parameters' });
+  } catch (err) {
+    res.status(400).type('json').send(err);
   }
 };
 
@@ -101,34 +116,29 @@ export const deleteModule = async (
         _id: req.params.moduleId
       });
 
+      if (!module) {
+        throw { message: 'Module with given id is not found in database' };
+      }
       //Deletes the problems in the problems array from the problems collection
       try {
         for (let i = 0; i < module.problems.length; i++) {
-          await Problem.findOneAndDelete({
+          const problem = await Problem.findOneAndDelete({
             _id: module.problems[i]
           });
+          if (!problem) {
+            throw { message: 'Problem with given id is not found in database' };
+          }
         }
       } catch (err) {
-        res
-          .status(400)
-          .type('json')
-          .send(err);
+        res.status(400).type('json').send(err);
+        return;
       }
+
       await module.delete();
-      
-      if (module != null) {
-        res
-          .status(200)
-          .type('json')
-          .send(
-            { message: 'Module successfully deleted' }
-          );
-      } else {
-        res
-          .status(404)
-          .type('json')
-          .send({ error: 'Module with given id is not found' });
-      }
+      res
+        .status(200)
+        .type('json')
+        .send({ message: 'Module successfully deleted' });
     }
   } catch (err) {
     res.status(400).type('json').send(err);
