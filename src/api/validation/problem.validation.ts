d3951a1ce1301f47be6700ff2ec935a542295827
Hamish Pierpont <44186660+HamishPierpont@users.noreply.github.com>
2021-10-11 17:18:21 -0400

    Added backend tests, fixed bugs (#54)
    * Moved problemModel.code.templatePackage to problemModel.templatePackage
    
    * Fixed server-crashing TypeError in problem.validation.ts
    
    * Refactored validation again to expect moduleName - create problem route now creates a problem in _problem collection, but doesn't update modules with the right entry
    
    * Implemented fix to problemController, removed minimum string length for problem.code fields, exported ModuleDocument type to permit type annotations
    
    * Put problem.templatePackage in the right place in the model, changed passnord to password, adding new problem works as tested by Postman
    
    * Commented out temporarily stale import
    
    * Problem creation tests are working with authentication
    
    * Problem creation tested, bugs in backend fixed, GET /student/problem(/{problemId}) tested
    
    * Changed moduleId and problemId from being on req.body to being on req.params in createProblem controller
    
    * Added tests for '/v1/(student|admin)/problem/findByModule/*
    
    * Tested getting by problemId
    
    * Added tests for problem deletion
    
    * Problem updating routes are working
    
    * Added checks for validity of moduleId field for createProblem

--- src/api/validation/problem.validation.ts
+++ src/api/validation/problem.validation.ts
@@ -1,30 +1,38 @@
-import * as Joi from 'joi';
+import {
+  ValidationResult,
+  object,
+  string,
+  boolean,
+  date,
+  array,
+  number
+} from 'joi';
 
 //eslint-disable-next-line
-const problemValidation = (data: any): Joi.ValidationResult => {
-  const schema = Joi.object({
-    moduleId: Joi.string().required(),
-    statement: Joi.string().required(),
-    title: Joi.string().min(1).required(),
-    hidden: Joi.boolean().required(),
-    language: Joi.string().min(1).required(),
-    dueDate: Joi.date().iso().required(),
+const problemValidation = (data: any): ValidationResult => {
+  const schema = object({
+    moduleId: string().min(24).max(24).required(),
+    statement: string().required(),
+    title: string().min(1).required(),
+    hidden: boolean().required(),
+    language: string().min(1).required(),
+    dueDate: date().iso().required(),
     code: {
-      header: Joi.string().allow('').required(),
-      body: Joi.string().allow('').required(),
-      footer: Joi.string().allow('').required()
+      header: string().allow('').required(),
+      body: string().allow('').required(),
+      footer: string().allow('').required()
     },
-    fileExtension: Joi.string().valid('.java', '.cpp', '.h').min(1).required(),
-    testCases: Joi.array().items({
-      input: Joi.string().min(1).required(),
-      expectedOutput: Joi.string().min(1).required(),
-      hint: Joi.string().min(1).required(),
-      visibility: Joi.number().valid(0, 1, 2).required()
+    fileExtension: string().valid('.java', '.cpp', '.h').min(1).required(),
+    testCases: array().items({
+      input: string().min(1).required(),
+      expectedOutput: string().min(1).required(),
+      hint: string().min(1).required(),
+      visibility: number().valid(0, 1, 2).required()
     }),
-    templatePackage: Joi.string().min(1).required(),
-    timeLimit: Joi.number(),
-    memoryLimit: Joi.number(),
-    buildCommand: Joi.string()
+    templatePackage: string().uri().min(1).required(),
+    timeLimit: number(),
+    memoryLimit: number(),
+    buildCommand: string()
   });
 
   return schema.validate(data);
