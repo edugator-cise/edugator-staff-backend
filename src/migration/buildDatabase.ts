039ba7082b1eacdf3461720ed2d39e361efbed07
Andrew Whigham <awhigham9@gmail.com>
2022-02-04 13:54:59 -0500

    Fixed imports to use TypeScript style correclty

--- src/migration/buildDatabase.ts
+++ src/migration/buildDatabase.ts
@@ -1,6 +1,6 @@
-import mysql from 'mysql2';
+import { createConnection, Connection } from 'mysql2';
 
-const connection: mysql.Connection = mysql.createConnection({
+const connection: Connection = createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
