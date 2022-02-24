678f11fd5d545da0042795cc686a5960674f96cc
John Dillon <john.dillon@ufl.edu>
2022-02-24 18:39:55 -0500

    WIP - changed rules to allow logging for mongo data exploration

--- src/migration/readFromMongo.ts
+++ src/migration/readFromMongo.ts
@@ -1,33 +1,45 @@
-// import * as dotenv from "dotenv";
-// import * as mongoDB from "mongodb";
+import { connect } from '../config/database';
+import { Module, ModuleInterface } from '../api/models/module.model';
+import { Problem, ProblemInterface } from '../api/models/problem.model';
 
-// export const collections: {modules?: mongoDB.Collection} = {}
-
-// export async function connectToDatabase () {
-//     dotenv.config();
-
-//     const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING);
-
-//     await client.connect();
-
-//     const db: mongoDB.Db = client.db(process.env.DB_NAME);
-
-//     const modulesCollection: mongoDB.Collection = db.collection(process.env.GAMES_COLLECTION_NAME);
-
-//     collections.games = gamesCollection;
-
-//     console.log(`Successfully connected to database: ${db.databaseName} and collection: ${gamesCollection.collectionName}`);
-//  }
-import {
-  Module,
-  ModuleDocument,
-  ModuleInterface
-} from '../api/models/module.model';
-
-const getModuleData = async (): Promise<void> => {
+const getModuleData = async (): Promise<ModuleInterface[]> => {
   let modules: ModuleInterface[];
   modules = await Module.find().select('-problems').sort({ number: 1 });
-  for (const module of modules) {
-    console.log(module.name);
-  }
+
+  return modules;
 };
+
+const getProblemData = async (): Promise<ProblemInterface[]> => {
+  let problems: ProblemInterface[];
+  problems = await Problem.find({});
+
+  return problems;
+};
+
+
+
+connect();
+
+const modulesPromise: Promise<ModuleInterface[]> = getModuleData();
+const problemsPromise: Promise<ProblemInterface[]> = getProblemData();
+
+const logModules = async () => {
+  const modulesArray: ModuleInterface[] = await modulesPromise;
+  for (const module of modulesArray) {
+    console.log(module);
+  }
+
+  console.log(modulesArray.length);
+};
+
+const logProblems = async () => {
+  const problemsArray: ProblemInterface[] = await problemsPromise;
+  for (const problem of problemsArray) {
+    console.log(problem);
+  }
+
+  console.log(problemsArray.length);
+};
+
+logModules();
+logProblems();
