0983227c86c6d789025c6f1973d2241f903c0424
John Dillon <john.dillon@ufl.edu>
2022-03-01 18:21:21 -0500

    working on E and T of the ETL

--- src/migration/readFromMongo.ts
+++ src/migration/readFromMongo.ts
@@ -1,12 +1,12 @@
 import { connect } from '../config/database';
 import { Module, ModuleInterface } from '../api/models/module.model';
-import { Problem, ProblemInterface } from '../api/models/problem.model';
-import {
-  mySQLCode,
-  mySQLModule,
-  mySQLProblem,
-  mySQLTestCase
-} from './mySQLTypes';
+// import { Problem, ProblemInterface } from '../api/models/problem.model';
+// import {
+//   mySQLCode,
+//   mySQLModule,
+//   mySQLProblem,
+//   mySQLTestCase
+// } from './mySQLTypes';
 
 // await this function inside another async in order to access the data.
 const getModuleData = async (): Promise<ModuleInterface[]> => {
@@ -17,30 +17,33 @@ const getModuleData = async (): Promise<ModuleInterface[]> => {
 };
 
 // await this function inside another async in order to access the data.
-const getProblemData = async (): Promise<ProblemInterface[]> => {
-  let problems: ProblemInterface[];
-  problems = await Problem.find({});
+// const getProblemData = async (): Promise<ProblemInterface[]> => {
+//   let problems: ProblemInterface[];
+//   problems = await Problem.find({});
 
-  return problems;
-};
+//   return problems;
+// };
 
 // connect to the mongo instance.
 connect();
 
 const modulesPromise: Promise<ModuleInterface[]> = getModuleData();
-const problemsPromise: Promise<ProblemInterface[]> = getProblemData();
+// const problemsPromise: Promise<ProblemInterface[]> = getProblemData();
 
-let mySQLModuleArray: mySQLModule[];
-let mySQLProblemArray: mySQLProblem[];
-let mySQLCodeArray: mySQLCode[];
-let mySQLTestCaseArray: mySQLTestCase[];
+// let mySQLModuleArray: mySQLModule[];
+// let mySQLProblemArray: mySQLProblem[];
+// let mySQLCodeArray: mySQLCode[];
+// let mySQLTestCaseArray: mySQLTestCase[];
 
-const transformData = async () => {
-  const mongoModulesArray = await modulesPromise;
-  const mongoProblemsArray = await problemsPromise;
+// const transformData = async () => {
+//   const mongoModulesArray = await modulesPromise;
+//   const mongoProblemsArray = await problemsPromise;
 
-  // TODO: here, parse out all of the data from mongo and insert accordingly into the new mySQL Arrays
-};
+//   // TODO: here, parse out all of the data from mongo and insert accordingly into the new mySQL Arrays
+
+//   // We can parse the modules out - need to keep track of the problems associated with a module.
+//   // Those will be inserted into the problems table along with FKs to modules.
+// };
 
 // TODO: we should then insert all of this data into the mysql database.
 
@@ -54,16 +57,16 @@ const logModules = async () => {
   console.log(modulesArray.length);
 };
 
-// for logging/data exploration
-const logProblems = async () => {
-  const problemsArray: ProblemInterface[] = await problemsPromise;
-  for (const problem of problemsArray) {
-    console.log(problem);
-  }
+// // for logging/data exploration
+// const logProblems = async () => {
+//   const problemsArray: ProblemInterface[] = await problemsPromise;
+//   for (const problem of problemsArray) {
+//     console.log(problem);
+//   }
 
-  console.log(problemsArray.length);
-};
+//   console.log(problemsArray.length);
+// };
 
 // logging
 logModules();
-logProblems();
+// logProblems();
