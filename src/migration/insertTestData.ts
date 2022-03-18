cbd8618f13e295e3212bf17ce43209e571e562fc
Andrew Whigham <awhigham9@gmail.com>
2022-03-18 11:04:46 -0400

    Add script for INSERTing test data into mysql

---
+++ src/migration/insertTestData.ts
@@ -0,0 +1,120 @@
+/* eslint-disable no-console */
+import { createConnection, Connection } from 'mysql2';
+
+const insertModules = async (connection: Connection) => {
+  console.log('Inserting into Module. . .');
+  connection.query(
+    `
+    INSERT INTO Module (name, number) VALUES
+      ('Test Module One', 1.0),
+      ('Test Module Two', 1.1)
+    `,
+    function (err) {
+      if (err) throw err;
+    }
+  );
+};
+
+const insertProblems = async (connection: Connection) => {
+  console.log('Inserting into Problem. . .');
+  connection.query(
+    `
+    INSERT INTO Problem 
+      (statement, title, hidden, language, due_date, file_extension,
+        template_package, time_limit, memory_limit, build_command,
+        module_id)
+    SELECT
+      'test statement 1',
+      'test title 1',
+      FALSE,
+      'cpp',
+      DATE('2022-12-31 01:00:00'),
+      '.cpp',
+      'test template_package 1',
+      1.00,
+      1.00,
+      'test build_command 1',
+      Module.id
+    FROM Module
+    WHERE name = 'Test Module One'
+    LIMIT 1
+    `,
+    function (err) {
+      if (err) throw err;
+    }
+  );
+  connection.query(
+    `
+    INSERT INTO Problem 
+      (statement, title, hidden, language, due_date, file_extension,
+        template_package, time_limit, memory_limit, build_command,
+        module_id)
+    SELECT
+      'test statement 2',
+      'test title 2',
+      FALSE,
+      'cpp',
+      DATE('2022-12-31 01:00:00'),
+      '.cpp',
+      'test template_package 2',
+      1.00,
+      1.00,
+      'test build_command 2',
+      Module.id
+    FROM Module
+    WHERE name = 'Test Module One'
+    LIMIT 1
+    `,
+    function (err) {
+      if (err) throw err;
+    }
+  );
+  connection.query(
+    `
+    INSERT INTO Problem 
+      (statement, title, hidden, language, due_date, file_extension,
+        template_package, time_limit, memory_limit, build_command,
+        module_id)
+    SELECT
+      'test statement 3',
+      'test title 3',
+      FALSE,
+      'cpp',
+      DATE('2022-12-31 01:00:00'),
+      '.cpp',
+      'test template_package 3',
+      1.00,
+      1.00,
+      'test build_command 3',
+      Module.id
+    FROM Module
+    WHERE name = 'Test Module One'
+    LIMIT 1
+    `,
+    function (err) {
+      if (err) throw err;
+    }
+  );
+};
+
+const runTest = async (): Promise<void> => {
+  const connection: Connection = createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+
+  connection.connect(function (err) {
+    if (err) throw err;
+  });
+
+  await insertModules(connection);
+  await insertProblems(connection);
+
+  connection.end(function (err) {
+    if (err) throw err;
+  });
+};
+
+runTest();
