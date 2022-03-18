a061e7de03b32b427f76f82def9a64a190ea0716
Andrew Whigham <awhigham9@gmail.com>
2022-03-18 11:03:46 -0400

    Change pool export to function for ownership of pool

--- src/migration/pool.ts
+++ src/migration/pool.ts
@@ -1,15 +1,16 @@
 import { Pool, createPool } from 'mysql2';
 
-// Create and export a shared connection pool for querying the db
+// Get a connection pool for querying the db
+function getPool(): Pool {
+  return createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: process.env.DB_POOL_WAIT_FOR_CONNECTIONS == 'true',
+    connectionLimit: parseInt(process.env.DB_POOL_CONNECTION_LIMIT),
+    queueLimit: parseInt(process.env.DB_POLL_QUEUE_LIMIT)
+  });
+}
 
-const pool: Pool = createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: process.env.DB_POOL_WAIT_FOR_CONNECTIONS == 'true',
-  connectionLimit: parseInt(process.env.DB_POOL_CONNECTION_LIMIT),
-  queueLimit: parseInt(process.env.DB_POLL_QUEUE_LIMIT)
-});
-
-export default pool;
+export default getPool;
