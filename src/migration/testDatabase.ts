383a9949229663d5b1cb68ec2de89447901a50ae
Andrew Whigham <awhigham9@gmail.com>
2022-03-19 09:08:24 -0400

    Refactor test script to import insert functions

--- src/migration/testDatabase.ts
+++ src/migration/testDatabase.ts
@@ -1,22 +1,9 @@
 /* eslint-disable no-console */
 import { createConnection, Connection } from 'mysql2';
 import { ProblemOrm } from './problem.orm';
+import { insertModules, insertProblems } from './insertTestData';
 
 const INSERT_DATA = true;
-
-const insertModules = async (connection: Connection) => {
-  console.log('Inserting into Module. . .');
-  connection.query(
-    `
-    INSERT INTO Module (name, number) VALUES
-      ('Test Module One', 1.0),
-      ('Test Module Two', 1.1)
-    `,
-    function (err) {
-      if (err) throw err;
-    }
-  );
-};
 
 const selectModules = async (connection: Connection) => {
   console.log('Selecting from Module. . .');
@@ -28,88 +15,6 @@ const selectModules = async (connection: Connection) => {
       } else {
         console.log(`Module Rows:\n${JSON.stringify(rows, null, 2)}`);
       }
-    }
-  );
-};
-
-const insertProblems = async (connection: Connection) => {
-  console.log('Inserting into Problem. . .');
-  connection.query(
-    `
-    INSERT INTO Problem 
-      (statement, title, hidden, language, due_date, file_extension,
-       template_package, time_limit, memory_limit, build_command,
-       module_id)
-    SELECT
-      'test statement 1',
-      'test title 1',
-      FALSE,
-      'cpp',
-      DATE('2022-12-31 01:00:00'),
-      '.cpp',
-      'test template_package 1',
-      1.00,
-      1.00,
-      'test build_command 1',
-      Module.id
-    FROM Module
-    WHERE name = 'Test Module One'
-    LIMIT 1
-    `,
-    function (err) {
-      if (err) throw err;
-    }
-  );
-  connection.query(
-    `
-    INSERT INTO Problem 
-      (statement, title, hidden, language, due_date, file_extension,
-       template_package, time_limit, memory_limit, build_command,
-       module_id)
-    SELECT
-      'test statement 2',
-      'test title 2',
-      FALSE,
-      'cpp',
-      DATE('2022-12-31 01:00:00'),
-      '.cpp',
-      'test template_package 2',
-      1.00,
-      1.00,
-      'test build_command 2',
-      Module.id
-    FROM Module
-    WHERE name = 'Test Module One'
-    LIMIT 1
-    `,
-    function (err) {
-      if (err) throw err;
-    }
-  );
-  connection.query(
-    `
-    INSERT INTO Problem 
-      (statement, title, hidden, language, due_date, file_extension,
-       template_package, time_limit, memory_limit, build_command,
-       module_id)
-    SELECT
-      'test statement 3',
-      'test title 3',
-      FALSE,
-      'cpp',
-      DATE('2022-12-31 01:00:00'),
-      '.cpp',
-      'test template_package 3',
-      1.00,
-      1.00,
-      'test build_command 3',
-      Module.id
-    FROM Module
-    WHERE name = 'Test Module One'
-    LIMIT 1
-    `,
-    function (err) {
-      if (err) throw err;
     }
   );
 };
