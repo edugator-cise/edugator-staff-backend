dcb702318c6d7a832ab6d0d3fd4d6d11a3c406f7
Andrew Whigham <awhigham9@gmail.com>
2022-03-25 11:31:27 -0400

    Add another SELECT construction test

--- __tests__/query.test.ts
+++ __tests__/query.test.ts
@@ -94,4 +94,46 @@ describe('constructSqlSelect', () => {
       )
     );
   });
+
+  it('Checks construction of SELECT from Problem with all props in filter and limit', () => {
+    expect(
+      lex(
+        constructSqlSelect(
+          Table.Problem,
+          {
+            _id: 1,
+            statement: 'test statement 2',
+            title: 'test title 2',
+            hidden: false,
+            language: 'cpp',
+            dueDate: new Date('2022-12-31T01:00:00'),
+            fileExtension: '.cpp',
+            templatePackage: 'test template_package 2',
+            timeLimit: 1.0,
+            memoryLimit: 1.0,
+            buildCommand: 'test build_command 2'
+          },
+          { limit: 5 }
+        )
+      )
+    ).toEqual(
+      lex(
+        `SELECT *
+        FROM \`Problem\`
+        WHERE
+        \`id\` = 1 AND
+        \`statement\` = 'test statement 2' AND
+        \`title\` = 'test title 2' AND
+        \`hidden\` = false AND
+        \`language\` = 'cpp' AND
+        \`due_date\` = '2022-12-31 01:00:00.000' AND
+        \`file_extension\` = '.cpp' AND
+        \`template_package\` = 'test template_package 2' AND
+        \`time_limit\` = 1 AND
+        \`memory_limit\` = 1 AND
+        \`build_command\` = 'test build_command 2'
+        LIMIT 5`
+      )
+    );
+  });
 });
