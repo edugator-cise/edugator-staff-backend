6c4ab57de78afe5b71622b58494b7405e042ca48
Andrew Whigham <awhigham9@gmail.com>
2022-04-06 09:32:19 -0400

    Add Module table build before tests

--- mocks/setup/setupAfterEnv.ts
+++ mocks/setup/setupAfterEnv.ts
@@ -1,12 +1,14 @@
 import * as databaseHelper from '../../src/config/database';
 import { buildUserTable } from '../../src/api/models/user.mysql.model';
 import { buildProblem } from '../../src/api/models/problem.mysql.model';
+import { buildModuleTable } from '../../src/api/models/module.mysql.model';
 //eslint-disable-next-line
 require('mysql2/node_modules/iconv-lite').encodingExists('cesu8');
 
 process.env.NODE_ENV = 'test';
 //eslint-disable-next-line
 beforeAll(async () => {
+  await buildModuleTable();
   await buildUserTable();
   await buildProblem();
   return databaseHelper.connect();
