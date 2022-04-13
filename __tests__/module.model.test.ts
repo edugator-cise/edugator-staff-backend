72b68adf7e3f711daccc3fdc7167181ac48b4bd0
John Dillon <john.dillon@ufl.edu>
2022-04-13 17:30:48 -0400

    working on module model test

--- __tests__/module.model.test.ts
+++ __tests__/module.model.test.ts
@@ -51,4 +51,43 @@ describe('Module Sequelize Model', () => {
     expect(modules[0].problems[0]).toMatchObject(createSampleProblemMySql());
     //eslint-disable-next-line
   });
+
+  it('checks whether querying by field on module works', async () => {
+    const modules = await ModuleTable.findAll({
+      where: { number: 5.1 },
+      include: [
+        {
+          model: ProblemTable,
+          as: 'problems',
+          include: [
+            { model: TestCaseTable, as: 'testCases' },
+            { model: CodeTable, as: 'code' }
+          ]
+        }
+      ]
+    });
+    expect(modules).toBeTruthy();
+    expect(modules.length).toBeGreaterThan(0);
+    expect(modules[0]).toMatchObject(createSampleModule());
+    expect(modules[0].problems.length).toBe(1);
+    expect(modules[0].problems[0]).toMatchObject(createSampleProblemMySql());
+  });
+
+  it('checks whether deleting all works', async () => {
+    await ModuleTable.destroy({ where: {} });
+    const modules = await ModuleTable.findAll({
+      include: [
+        {
+          model: ProblemTable,
+          as: 'problems',
+          include: [
+            { model: TestCaseTable, as: 'testCases' },
+            { model: CodeTable, as: 'code' }
+          ]
+        }
+      ]
+    });
+
+
+  })
 });
