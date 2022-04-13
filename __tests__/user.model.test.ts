fb33a1cbf7debd7d8af34e2e1bf2cb36e74ec667
John Dillon <john.dillon@ufl.edu>
2022-04-12 20:42:41 -0400

    adds delete and update tests for User

--- __tests__/user.model.test.ts
+++ __tests__/user.model.test.ts
@@ -1,6 +1,8 @@
 import { UserTable } from '../src/api/models/user.mysql.model';
 import * as bcrypt from 'bcrypt';
 
+jest.setTimeout(10000);
+
 describe('User Sequelize Model', () => {
   const sampleUser = {
     name: 'Test TA',
@@ -8,6 +10,13 @@ describe('User Sequelize Model', () => {
     role: 'TA',
     salt: 1
   };
+
+  const updatedUser = {
+    name: 'Test TA',
+    username: 'a new username',
+    role: 'TA',
+    salt: 1
+  }
 
   beforeEach(async () => {
     const pass = 'password';
@@ -49,5 +58,57 @@ describe('User Sequelize Model', () => {
     expect(users).toBeTruthy();
     expect(users.length).toBeGreaterThan(0);
     expect(users[0]).toMatchObject(sampleUser);
+  });
+
+  it('checks whether deleting all rows works', async () => {
+    let users = null;
+    try {
+      await UserTable.destroy({ where: {} });
+      users = await UserTable.findAll();
+    } catch (err) {
+      //eslint-disable-next-line
+      console.log(err);
+    }
+    expect(users).toBeTruthy();
+    expect(users.length).toBe(0);
+  });
+
+  it('checks whether deleting by username works', async () => {
+    let users = null;
+    try {
+      users = await UserTable.findAll();
+      expect(users).toBeTruthy();
+      expect(users.length).toBeGreaterThan(0);
+      expect(users[0]).toMatchObject(sampleUser);
+      await UserTable.destroy({ where: { username: users[0].username } });
+      users = await UserTable.findAll();
+    } catch (err) {
+      //eslint-disable-next-line
+      console.log(err);
+    }
+    expect(users).toBeTruthy();
+    expect(users.length).toBe(0);
+  });
+
+  it('checks whether updating a field works', async () => {
+    let originalUsers = null;
+    let updatedUsers = null;
+    try {
+      originalUsers = await UserTable.findAll();
+      expect(originalUsers).toBeTruthy();
+      expect(originalUsers.length).toBeGreaterThan(0);
+      expect(originalUsers[0]).toMatchObject(sampleUser);
+      await UserTable.update(
+        { username: 'a new username' },
+        { where: { username: originalUsers[0].username } }
+      );
+      updatedUsers = await UserTable.findAll();
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+    }
+    expect(updatedUsers).toBeTruthy();
+    expect(updatedUsers.length).toBeGreaterThan(0);
+    expect(updatedUsers[0]).toMatchObject(updatedUser);
   });
 });
