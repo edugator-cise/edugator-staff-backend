8c5327ea95b2f6966d92db257e9fa867beac64d2
John Dillon <john.dillon@ufl.edu>
2022-04-18 00:09:20 -0400

    working on translate functions

--- src/api/controllers/util.ts
+++ src/api/controllers/util.ts
@@ -1,4 +1,5 @@
 import { IModule } from '../models/module.mysql.model';
+import { IProblem, TestCase, Code } from '../models/problem.mysql.model';
 
 function translateIdOnModule(module: IModule) {
   return {
@@ -7,7 +8,7 @@ function translateIdOnModule(module: IModule) {
     name: module.name,
     number: module.number,
     problems: module.problems
-  }
+  };
 }
 
 function translateIdOnModuleArray(modules: IModule[]) {
@@ -18,4 +19,67 @@ function translateIdOnModuleArray(modules: IModule[]) {
   return newModules;
 }
 
-export { translateIdOnModule };
+function translateIdOnProblem(problem: IProblem) {
+  // how to do naming? camel or underscores?
+  return {
+    id: problem.id,
+    _id: problem.id,
+    statement: problem.statement,
+    title: problem.title,
+    hidden: problem.hidden,
+    language: problem.language,
+    due_date: problem.dueDate,
+    file_extension: problem.fileExtension,
+    template_package: problem.templatePackage,
+    time_limit: problem.timeLimit,
+    memory_limit: problem.memoryLimit,
+    build_command: problem.buildCommand,
+    module_id: problem.moduleId,
+    code: problem.code,
+    test_cases: problem.testCases
+  };
+}
+
+function translateIdOnProblemArray(problems: IProblem[]) {
+  const newProblems: any[] = [];
+  for (const problem of problems) {
+    newProblems.push(translateIdOnProblem(problem));
+  }
+  return newProblems;
+}
+
+// started working on this, but we don't even need it I guess? since _id works for user? sorta?
+// leaving untill we see more clearly on testing
+// function translateIdOnUser(user: IUser) {
+// }
+
+// function translateIdOnUserArray(users: IUser[]) {
+//   const newUsers: any[] = [];
+//   for (const user of users) {
+//     newUsers.push(transl)
+//   }
+// }
+
+// I don't even think this is needed, judge is the only service that calls directly so?
+// function translateIdOnCode(code: Code) {
+//   return {
+//     id: code.
+//   };
+// }
+
+// Same here - I don't think we need this, do we?
+// function translateIdOnTestCase(testCase: TestCase) {
+//   return {
+//     id: testCase.id
+//   };
+// }
+
+// function translateIdOnTestCaseArray(testCases: ) {
+//   const newTestCases: any[] = [];
+//   for (const testCase of testCases) {
+//     newTestCases.push(translateIdOnTestCase(testCase));
+//   }
+//   return newTestCases;
+// }
+
+export { translateIdOnModule, translateIdOnModuleArray };
