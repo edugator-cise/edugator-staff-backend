3161c7cd138c948b2afec0642f6a9be7e895bdc9
Marc Diaz <33824219+marcgabe15@users.noreply.github.com>
2023-06-26 00:35:29 -0700

    add db user and pass secrets (#146)
    * add db user and pass secrets
    
    * utils and add new mongoid
    
    * prettier

---
+++ src/util.ts
@@ -0,0 +1,13 @@
+export default function assertString(input) {
+  const isString = typeof input === 'string' || input instanceof String;
+
+  if (!isString) {
+    let invalidType = typeof input;
+    if (invalidType === 'object') invalidType = input.constructor.name;
+    throw new TypeError(`Expected a string but received a ${invalidType}`);
+  }
+}
+export const isMongoId = (str: string) => {
+  assertString(str);
+  return /^(0x|0h)?[0-9A-F]+$/i.test(str) && str.length === 24;
+};
