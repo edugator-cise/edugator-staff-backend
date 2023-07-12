0f9fd687c08c93500c12439a26f8a58764914655
Prayuj Tuli <82406637+prayujt@users.noreply.github.com>
2023-07-12 15:03:12 -0400

    added basic v2 routes (#155)

--- src/api/routes/v2/index.ts
+++ src/api/routes/v2/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { courseRouter } from './course.routes';
 import { organizationRouter } from './organization.routes';
 import { moduleRouter } from './module.routes';
+import { codeRouter } from './code.routes';
 import { studentProblemRouter, adminProblemRouter } from './problem.routes';
 import { studentLessonRouter, adminLessonRouter } from './lesson.routes';
 
@@ -10,6 +11,7 @@ const router = Router();
 router.use('/course', courseRouter);
 router.use('/organization', organizationRouter);
 router.use('/module', moduleRouter);
+router.use('/code/run', codeRouter);
 router.use('/student/problem', studentProblemRouter);
 router.use('/admin/problem', adminProblemRouter);
 router.use('/student/lesson', studentLessonRouter);
