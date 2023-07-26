4e31bca1a7e9b42b181e3f562ab06c61b5d85459
Marc Diaz <marcgabe15@gmail.com>
2023-07-25 17:33:13 -0700

    add swagger

---
+++ src/api/util/swagger.ts
@@ -0,0 +1,40 @@
+import { Request, Response, Express } from 'express';
+import swaggerJSdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import { version } from '../../../package.json';
+
+
+const swaggerDefinition: swaggerJSdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'REST API Docs',
+      version: version,
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['./src/api/routes/v1/health.routes.ts'],
+};
+
+const swaggerSpec = swaggerJSdoc(swaggerDefinition);
+
+export function swaggerDocs(app:Express, _port: number) {
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/docs.json", (_req: Request, res: Response) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  })
+};
--- src/config/express.ts
+++ src/config/express.ts
@@ -1,9 +1,9 @@
-import * as express from 'express';
-import { Request, Response } from 'express';
+import express from 'express';
+import { Request, Response, Express } from 'express';
 import routes from '../api/routes/v1';
 import routesV2 from '../api/routes/v2';
-import * as cors from 'cors';
-import * as passport from 'passport';
+import cors from 'cors';
+import passport from 'passport';
 import { jwtStrategy } from './passport';
 import * as database from './database';
 import * as databasev2 from './database_v2';
@@ -12,11 +12,13 @@ import { Organization } from '../api/models/v2/organization.model';
 import { Module } from '../api/models/v2/module.model';
 import { Problem, TestCase } from '../api/models/v2/problem.model';
 import { Lesson } from '../api/models/v2/lesson.model';
+import { swaggerDocs } from '../api/util/swagger';
 
 class Server {
-  public app: express.Application;
-
+  public app: Express;
+  public port: number;
   constructor() {
+    this.port = 8080;
     this.app = express();
     this.connectDatabase();
     if (process.env.NODE_ENV !== 'test') {
@@ -28,6 +30,7 @@ class Server {
   public routes(): void {
     this.app.use('/v1', routes);
     this.app.use('/v2', routesV2);
+    swaggerDocs(this.app, this.port);  
     this.app.get('/', (_req: Request, res: Response): void => {
       // use static 200 to prevent undefined message from http-status
       res.status(200).send('OK');
@@ -60,7 +63,9 @@ class Server {
 
   public start(): void {
     //eslint-disable-next-line
-    this.app.listen(8080, () => console.log(`server started on port 8080`));
+    this.app.listen(this.port, () => {
+      console.log(`server started on port 8080`)    
+    });
   }
 }
 
