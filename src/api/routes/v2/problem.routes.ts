dcee6d1e746a5307d98828c342c1c6fe75f529c9
Prayuj Tuli <prayujtuli@hotmail.com>
2023-07-27 21:18:27 -0400

    cleaned up code

--- src/api/routes/v2/problem.routes.ts
+++ src/api/routes/v2/problem.routes.ts
@@ -1,27 +1,25 @@
 import * as express from 'express';
 
 import {
+  getStudentProblem,
   createProblem,
-  readAdminProblems,
+  getAdminProblem,
   updateProblem,
-  deleteProblem,
-  readStudentProblems
+  deleteProblem
 } from '../../controllers/v2/problem.controller';
 
 const adminProblemRouter = express.Router();
 const studentProblemRouter = express.Router();
 
 // Student routes
-studentProblemRouter.route('/:problemId').get(readStudentProblems);
-studentProblemRouter.route('/findByModule/:moduleId').get(readStudentProblems);
+studentProblemRouter.route('/:problemId').get(getStudentProblem);
 
 // Admin routes
-adminProblemRouter.route('/').get(readAdminProblems).post(createProblem);
+adminProblemRouter.route('/').post(createProblem);
 adminProblemRouter
   .route('/:problemId')
-  .get(readAdminProblems)
+  .get(getAdminProblem)
   .put(updateProblem)
   .delete(deleteProblem);
-adminProblemRouter.route('/findByModule/:moduleId').get(readAdminProblems);
 
 export { adminProblemRouter, studentProblemRouter };
