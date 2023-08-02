58698b9af231ac9717e6c9894fa12070c164ee27
Marc Diaz <33824219+marcgabe15@users.noreply.github.com>
2023-08-01 18:59:49 -0700

    add swagger (#158)
    * add swagger
    
    * add swagger work
    
    * delete multer and tsconfig
    
    * swagger-jsdocs
    
    * swagger

--- src/api/routes/v1/health.routes.ts
+++ src/api/routes/v1/health.routes.ts
@@ -2,6 +2,17 @@ import * as express from 'express';
 import { getHealth } from '../../controllers/v1/health.controller';
 const router = express.Router();
 
+/**
+ * @openapi
+ * /health:
+ *  get:
+ *     tags:
+ *     - Healthcheck
+ *     description: Responds if the app is up and running
+ *     responses:
+ *       200:
+ *         description: App is up and running
+ */
 router.route('/').get(getHealth);
 
 export default router;
--- src/config/express.ts
+++ src/config/express.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
+import { Request, Response, Express } from 'express';
 import routes from '../api/routes/v1';
 import routesV2 from '../api/routes/v2';
 import * as cors from 'cors';
@@ -12,11 +12,13 @@ import { Organization } from '../api/models/v2/organization.model';
 import { Module } from '../api/models/v2/module.model';
 import { Problem, TestCase } from '../api/models/v2/problem.model';
 import { Lesson } from '../api/models/v2/lesson.model';
+import { swaggerDocs } from '../api/util/swagger';
 
 class Server {
-  public app: express.Application;
-
+  public app: Express;
+  public port: number;
   constructor() {
+    this.port = 8080;
     this.app = express();
     this.connectDatabase();
     if (process.env.NODE_ENV !== 'test') {
@@ -28,6 +30,7 @@ class Server {
   public routes(): void {
     this.app.use('/v1', routes);
     this.app.use('/v2', routesV2);
+    swaggerDocs(this.app, this.port);
     this.app.get('/', (_req: Request, res: Response): void => {
       // use static 200 to prevent undefined message from http-status
       res.status(200).send('OK');
@@ -59,8 +62,10 @@ class Server {
   }
 
   public start(): void {
-    //eslint-disable-next-line
-    this.app.listen(8080, () => console.log(`server started on port 8080`));
+    this.app.listen(this.port, () => {
+      //eslint-disable-next-line
+      console.log(`server started on port 8080`);
+    });
   }
 }
 
