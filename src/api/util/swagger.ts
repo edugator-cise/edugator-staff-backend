505a5e9d319d62410aabac83999d61cf6f045150
Marc Diaz <marcgabe15@gmail.com>
2023-08-01 17:48:07 -0700

    add swagger work

--- src/api/util/swagger.ts
+++ src/api/util/swagger.ts
@@ -3,38 +3,37 @@ import swaggerJSdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import { version } from '../../../package.json';
 
-
 const swaggerDefinition: swaggerJSdoc.Options = {
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'REST API Docs',
-      version: version,
+      version: version
     },
     components: {
       securitySchemes: {
         bearerAuth: {
           type: 'http',
           scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
+          bearerFormat: 'JWT'
+        }
+      }
     },
     security: [
       {
-        bearerAuth: [],
-      },
-    ],
+        bearerAuth: []
+      }
+    ]
   },
-  apis: ['./src/api/routes/v1/health.routes.ts'],
+  apis: ['./src/api/routes/v1/health.routes.ts']
 };
 
 const swaggerSpec = swaggerJSdoc(swaggerDefinition);
 
-export function swaggerDocs(app:Express, _port: number) {
+export function swaggerDocs(app: Express, _port: number) {
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  app.get("/docs.json", (_req: Request, res: Response) => {
-    res.setHeader("Content-Type", "application/json");
+  app.get('/docs.json', (_req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
     res.send(swaggerSpec);
-  })
-};
+  });
+}
--- src/config/express.ts
+++ src/config/express.ts
@@ -30,7 +30,7 @@ class Server {
   public routes(): void {
     this.app.use('/v1', routes);
     this.app.use('/v2', routesV2);
-    swaggerDocs(this.app, this.port);  
+    swaggerDocs(this.app, this.port);
     this.app.get('/', (_req: Request, res: Response): void => {
       // use static 200 to prevent undefined message from http-status
       res.status(200).send('OK');
@@ -62,9 +62,9 @@ class Server {
   }
 
   public start(): void {
-    //eslint-disable-next-line
     this.app.listen(this.port, () => {
-      console.log(`server started on port 8080`)    
+      //eslint-disable-next-line
+      console.log(`server started on port 8080`);
     });
   }
 }
