0c85acafddc5768dd505fc589b0e089d7d2dd4d5
Marc Diaz <marcgabe15@gmail.com>
2023-08-27 17:46:35 -0700

    update yml configuration

--- src/config/vars.ts
+++ src/config/vars.ts
@@ -12,20 +12,11 @@ export const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
 export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 export const DATABASE_HOST = process.env.DATABASE_HOST;
 
-//AWS batch variables
-export const jobDefinition = process.env.JOB_DEFINITION;
-export const jobQueue = process.env.JOB_QUEUE;
-
 // AWS batch environment
-export const BUCKET_NAME = process.env.UPLOAD_SUBMISSIONS_BUCKET;
-export const EDUGATOR_API_USER = process.env.EDUGATOR_API_USER;
 export const EDUGATOR_API_PASS = process.env.EDUGATOR_API_PASS;
-export const EDUGATOR_API_URL = process.env.EDUGATOR_API_URL;
 export const REGION_AWS = process.env.REGION_AWS;
 
 //Email user and pass
-export const SENDER_EMAIL = process.env.SENDER_EMAIL;
-export const APP_PASSWORD = process.env.APP_PASSWORD;
 
 //AUTH token for EDUGATOR
 export const EDUGATOR_AUTH_TOKEN = process.env.EDUGATOR_AUTH_TOKEN;
